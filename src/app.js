1c795b9224beb582d6029f502e35c0bb312a48ea
giusousa <giuliano.sousa11@gmail.com>
2020-10-17 23:45:30 -0300

    up

--- src/app.js
+++ src/app.js
@@ -3,29 +3,24 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const routes = require('./routes');
-
-
 const app = express();                  // Cria uma variável para armazenar a aplicação (instanciar a aplicação)
 
-app.use(cookieParser());
 
 const corsOptions = {
   origin: function (origin, callback) {
+
     const whitelist = [
-        'https://boimanso-frontend-7scc3zwlma-uc.a.run.app',
-        'https://easychat-backend-cn4eogsveq-uc.a.run.app'
+        'https://easychat-backend-cn4eogsveq-uc.a.run.app',
     ];
-
-
-    console.log('origin')
+    
     console.log(origin)
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if (!origin || whitelist.indexOf(origin) !== -1) 
+        return callback(null, true);
 
-
-    if (whitelist.indexOf(origin) !== -1 || (!origin &&  process.env.NODE_ENV === 'DEV')) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+    return callback(new Error(msg), false);
   }
 };
 
@@ -36,6 +31,22 @@ app.use(function customErrorHandler(err, req, res, next) {
     res.status(400).send(err.stack.split('(')[0]);
 });
 
+
+app.use( function (req, res, next) {
+
+    console.log('================')
+    console.log(req.get('host'))
+    console.log(req.get('origin'))
+
+
+
+
+
+    console.log('Time:', Date.now());
+    next();
+});
+
+app.use(cookieParser());
 app.use(express.json());                // Informa ao express que usaremos Json nas requisições ao servidor
 app.use(routes);                        // Faz com que o app utilize um arquivo externo que contem as rotas disponíveis
 app.use(errors());
