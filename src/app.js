1318b10173e8f9537bf7f4d5bd0e6684345c010d
giusousa <giuliano.sousa11@gmail.com>
2020-10-18 00:20:12 -0300

    up

--- src/app.js
+++ src/app.js
@@ -3,52 +3,17 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const routes = require('./routes');
+
 const app = express();                  // Cria uma variável para armazenar a aplicação (instanciar a aplicação)
 
+// O cors limita quais domínios (front-end) podem acessar a Api
 
-const corsOptions = {
-  origin: function (origin, callback) {
-
-    const whitelist = [
-        'https://easychat-backend-cn4eogsveq-uc.a.run.app',
-    ];
-    
-    console.log(origin)
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if (!origin || whitelist.indexOf(origin) !== -1) 
-        return callback(null, true);
-
-    const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-    return callback(new Error(msg), false);
-  }
-};
-
-// O cors limita quais domínios podem acessar a Api
-app.use(cors(corsOptions));
-// Caso tenha ocorrido algum erro com o 'CORS', retornar o erro
-app.use(function customErrorHandler(err, req, res, next) {
-    res.status(400).send(err.stack.split('(')[0]);
-});
-
-
-app.use( function (req, res, next) {
-
-    console.log('================')
-    console.log(req.get('host'))
-    console.log(req.get('origin'))
-    console.log('================')
-    console.log(req.header('Host'))
-    console.log(req.header('Origin'))
-
-
-
-
-
-
-    console.log('Time:', Date.now());
-    next();
-});
+// Utilizado quando quem está acionando esta API é um navegador.
+// O CORS permite que um site acesse recursos de um servidor que esteja em um domínio diferente.
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://boimanso-frontend-7scc3zwlma-uc.a.run.app'],
+    credentials: true
+}));
 
 app.use(cookieParser());
 app.use(express.json());                // Informa ao express que usaremos Json nas requisições ao servidor
