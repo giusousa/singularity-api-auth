0f6e0d1f1c0d68a1fc2ca561217c454639e16ccb
giusousa <giuliano.sousa11@gmail.com>
2020-10-27 18:13:23 -0300

    up

--- src/controllers/EmailCheck.js
+++ src/controllers/EmailCheck.js
@@ -11,10 +11,15 @@ module.exports = async (req, res) => {
     if (data[0].level === 'admin' || data[0].level === 'supermanager')
         return res.status(400).send({error: 'User unavailable for this function'}) 
 
+    console.log(data)
+
+
     const managersUser = await data.reduce(async (acc, { managerId }) => {
         const  [{name, managerName}] = await Mongo.index(res, schema, { managerId }, 'name managerName');
-        acc.push({managerId, managerName: managerName || name});
-        return acc;
+        console.log(name)
+        console.log(managerName)
+        console.log([...acc, {managerId, managerName: managerName || name}])
+        return [...acc, {managerId, managerName: managerName || name}]
     }, [])
 
     res.send(managersUser);
