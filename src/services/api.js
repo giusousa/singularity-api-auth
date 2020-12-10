9b62856bbc92a6bea8f1568c0564fb6a878e2966
giusousa <giuliano.sousa11@gmail.com>
2020-12-10 17:28:43 -0300

    up

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 const axios = require('axios') ;
+const port = process.env.NODE_ENV == 'DEV' ? 3333 : 8080
 
 module.exports = axios.create({
-    baseURL: 'http://localhost:3333/'
+    baseURL: `http://localhost:${port}/`
 })
